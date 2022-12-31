8d8b5df163e6c4d7ef21d4f5ea2142f86d61dfba
david-borja <davidborjamartinez@gmail.com>
2022-12-31 02:37:48 +0100

    add: flask setup

---
+++ app.py
@@ -0,0 +1,6 @@
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+
+print('Flask app running!')
